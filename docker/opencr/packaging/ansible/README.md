dc47f64a3ed5e7467247f635fb89158a5b4ba7d0
Piotr Mankowski <pmanko@uw.edu>
2021-10-13 23:53:25 +0000

    Major cleanup of old source code

--- docker/opencr/packaging/ansible/README.md
+++
@@ -1,3 +0,0 @@
-# Ansible
-
-The latest version of these docs are at: [https://intrahealth.github.io/client-registry-docs/dev/ansible/](https://intrahealth.github.io/client-registry-docs/dev/ansible/)
--- docker/opencr/server/lib/config.js
+++
@@ -1,7 +0,0 @@
-const nconf = require('nconf');
-const env = process.env.NODE_ENV || 'development';
-nconf.argv()
-  .env()
-  .file(`${__dirname}/../config/config_${env}.json`)
-  .file('decRules', `${__dirname}/../config/decisionRules.json`);
-module.exports = nconf;
