1a5c3b52a9c45dc2a7415a89093e975a95ea7e0b
Piotr Mankowski <pmanko@uw.edu>
2021-12-07 11:19:52 -0800

    CI fixes

--- .postman/run-tests.sh
+++ .postman/run-tests.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+docker-compose -f docker-compose.ports.yml up -d nginx openhim-core openhim-console mongo-db openhim-config
+sleep 10
+docker-compose -f docker-compose.ports.yml logs openhim-config
+docker-compose -f docker-compose.ports.yml up -d shr-fhir opencr-fhir opencr-es kafka zookeeper
+sleep 30
+docker-compose -f docker-compose.ports.yml up -d shr opencr
+sleep 30
+docker-compose -f docker-compose.ports.yml logs shr opencr
+
 collections=(
   'https://www.getpostman.com/collections/46fd37386092a9f460e4'
   'https://www.getpostman.com/collections/4d682cbb222bb538d365'
@@ -10,5 +19,5 @@ collections=(
 for collection in ${collections[@]}; do
   echo $collection
   export POSTMAN_COLLECTION=$collection
-  sudo -E docker-compose -f docker-compose.ports.yml up newman
+  docker-compose -f docker-compose.ports.yml up newman
 done
